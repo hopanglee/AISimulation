08c6dab397e3d87945d2e332a1e8e97841debbde
hopanglee <hgkinm87j@naver.com>
2025-03-10 13:40:13 +0900

    feat: Props of House set

---
+++ Assets/02.Scripts/Entity/Block/Props/Toilet.cs
@@ -0,0 +1,12 @@
+public class Toilet : Prop
+{
+    public override string Interact(Actor actor)
+    {
+        return default;
+    }
+
+    public override string Get()
+    {
+        throw new System.NotImplementedException();
+    }
+}
