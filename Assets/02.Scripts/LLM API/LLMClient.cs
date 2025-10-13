99e0d0c85f3e57a2f5f10fe53f5affe5f37cb1cd
hopanglee <hgkinm87j@naver.com>
2025-10-13 22:30:07 +0900

    hashkey 수정

--- Assets/02.Scripts/LLM API/LLMClient.cs
+++ Assets/02.Scripts/LLM API/LLMClient.cs
@@ -56,13 +56,7 @@ public abstract class LLMClient
     // 각 LLM 구현체에서 캐시 키 생성을 위해 필요한 객체를 반환합니다.
     protected virtual object GetHashKey()
     {
-        var lookable = actor.sensor.GetLookableEntities();
-        var str = "";
-        foreach (var kv in lookable)
-        {
-            str += $"{kv.Key} {kv.Value.Get()}\n";
-        }
-        str += actor.LoadCharacterInfo() + actor.LoadCharacterMemory();
+        var str = actor.LoadCharacterInfo() + actor.LoadCharacterMemory() + actor.LoadActorSituation();
         return str;
     }
     #region 메시지 관리
